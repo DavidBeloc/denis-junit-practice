299fe5ab9fb7b8250a1c577a46901ed90d01ed0c
David Belotserkovsky <d.belotserkovsky@gmail.com>
2023-12-06 18:48:35 +0300

    add LocalDateFormatter class tests

--- src/test/java/com/dmdev/util/LocalDateFormatterTest.java
+++ src/test/java/com/dmdev/util/LocalDateFormatterTest.java
@@ -1,10 +1,18 @@
 package com.dmdev.util;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 class LocalDateFormatterTest {
 
@@ -14,6 +22,30 @@ class LocalDateFormatterTest {
 
         LocalDate actualResult = LocalDateFormatter.format(date);
 
+        assertThat(actualResult).isEqualTo(LocalDate.of(2020, 11, 27));
     }
 
+    @Test
+    void throwDateTimeExceptionIfDateInvalid() {
+        String date = "123";
+
+        assertThrows(DateTimeParseException.class, () -> LocalDateFormatter.format(date));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getValidationArguments")
+    void isValid(String date, boolean expectedResult) {
+        boolean actualResult = LocalDateFormatter.isValid(date);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    static Stream<Arguments> getValidationArguments() {
+        return Stream.of(
+                Arguments.of("2020-11-27", true),
+                Arguments.of("01-01-2020", false),
+                Arguments.of("01-01-2020 12:25", false),
+                Arguments.of(null, false)
+        );
+    }
 }
